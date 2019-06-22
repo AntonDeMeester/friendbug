b55af7f15ba7d9608d48f4cb01da01a75533f0b1
Anton <antondm@hotmail.com>
2019-06-22 15:30:36 +0200

    Add correct database configuration. First working version

--- cmd/friendbug/main.go
+++ cmd/friendbug/main.go
@@ -1,18 +1,110 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/joho/godotenv"
 
 	// Own packages
-	client "friendbug/internal"
+	"friendbug/internal"
 )
 
+//TODO TO BE TRANSFERED TO SETTINGS
+const maxNumberOfContactsPerDay = 3
+const dateFormat = "2006-01-02"
+const targetNumber = "+32496952214"
+
 func main() {
-	err := godotenv.Load("../../.env")
+	err := godotenv.Load(".env")
 	if err != nil {
 		panic(err)
 	}
 
-	client.ExampleRedis()
-	
+	client := internal.GetDatabase()
+	friends := client.GetAllFriends()
+
+	toContactFriends := []internal.Friend{}
+
+	for _, friend := range friends {
+		if friend.DateContacted.Before(time.Now().AddDate(0, 0, -1)) {
+			toContactFriends = append(toContactFriends, friend)
+		}
+	}
+
+	contactedFriends := sendReminderForFriends(toContactFriends)
+	sendMessage(contactedFriends)
+	for _, friend := range contactedFriends {
+		client.SetFriendAsContacted(friend)
+	}
+}
+
+func sendReminderForFriends(friends []internal.Friend) (toContactFriends []internal.Friend) {
+	toSendRemindersFor := []internal.Friend{}
+	// If we have more friends than the max number to cintact, we need to choose from them
+	if len(friends) > maxNumberOfContactsPerDay {
+		selectedFriend := internal.Friend{}
+		for i := 0; i < maxNumberOfContactsPerDay; i++ {
+			selectedFriend, friends = selectRandomFriendWeighted(friends)
+			toSendRemindersFor = append(toSendRemindersFor, selectedFriend)
+		}
+	} else {
+		// If we have less than the max amount, we just add them all
+		for _, friend := range(friends) {
+			toSendRemindersFor = append(toSendRemindersFor, friend)
+		}
+	}
+
+	return toSendRemindersFor
+
+}
+
+func selectRandomFriendWeighted(friends []internal.Friend) (selectedFriend internal.Friend, remainingFriends []internal.Friend) {
+	if len(friends) == 0 {
+		return internal.Friend{}, friends
+	}
+
+	totalWeight := 0.0
+	for _, friend := range friends {
+		totalWeight += 360 / float64(friend.ContactFrequency)
+	}
+
+	selected := rand.Float64() * totalWeight
+	for i, friend := range friends {
+		selected -= 360 / float64(friend.ContactFrequency)
+		if selected < 0 {
+			// Save the friend
+			selectedFriend = friend
+			// Copy the last element to the place of the previous friend
+			friends[i] = friends[len(friends)-1]
+			// Remove the last element of the friends list
+			friends = friends[:len(friends)-1]
+
+			return selectedFriend, friends
+		}
+	}
+	// If we somehow don't find one, we return the last one
+	return friends[len(friends)], friends[:len(friends)-1]
+}
+
+func sendMessage(friends []internal.Friend) {
+	if len(friends) == 0 {
+		return
+	}
+
+	length := len(friends)
+	friendsString := ""
+	for i, friend := range friends {
+		friendsString += friend.Name
+		if i < length-2 {
+			friendsString += ", "
+		} else if i == length-2 {
+			friendsString += " and "
+		}
+	}
+
+	sendMessage := "Hey Anton, you should probably contact " + friendsString + " today!"
+	fmt.Println(sendMessage)
+	//internal.SendMessageTwilio(sendMessage, targetNumber)
 }
--- internal/database.go
+++ internal/database.go
@@ -1,31 +1,34 @@
 package internal
 
 import (
-	"os"
-	"github.com/go-redis/redis"
-	"time"
-	"fmt"
 	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+	"strconv"
+
+	"github.com/go-redis/redis"
 )
 
 const redisFriendListName = "friends"
-const dateFormat = "200T6-01-02"
+const dateFormat = "2006-01-02"
 
 // TODO Remove
 const data = `
 {
-	"name": Anton",
+	"name": "Anton",
 	"dateContacted": "2019-06-19",
-	"contactFrequency": "1"
+	"contactFrequency": 1
 }
 `
-
 
 // Because Golang doesn't work nicely with easy time formats for some reason
 type MyTime time.Time
 
-func (m *MyTime) UnmarshalJSON(p []byte) error {
-	t, err := time.Parse(dateFormat, string(p))
+func (m *MyTime) UnmarshalJSON(b []byte) error {
+	s := strings.Trim(string(b), "\"")
+	t, err := time.Parse(dateFormat, s)
 
 	if err != nil {
 		return err
@@ -36,13 +39,35 @@ func (m *MyTime) UnmarshalJSON(p []byte) error {
 	return nil
 }
 
-type Friend struct {
-	Name 				string 		`json:"name"`
-	DateContacted 		MyTime	 	`json:"dateContacted"`
-	ContactFrequency 	int 		`json:"contactFrequency"`
+func (m MyTime) MarshalJSON() ([]byte, error) {
+	time := m.Format(dateFormat)
+	time = strconv.Quote(time)
+	return []byte(time), nil
 }
 
-func (f Friend) GetData() []byte  {
+func (m MyTime) Before(t time.Time) bool {
+	mt := time.Time(m)
+	return mt.Before(t)
+}
+
+func (m MyTime) AddDays(days int) time.Time {
+	t := time.Time(m)
+	return t.AddDate(0, 0, days)
+}
+
+func (m MyTime) Format(format string) string {
+	t := time.Time(m)
+	return t.Format(format)
+}
+
+type Friend struct {
+	id               int64
+	Name             string `json:"name"`
+	DateContacted    MyTime `json:"dateContacted"`
+	ContactFrequency int    `json:"contactFrequency"`
+}
+
+func (f Friend) GetData() []byte {
 	value, err := json.Marshal(f)
 	if err != nil {
 		panic(err)
@@ -60,19 +85,23 @@ func NewFriendFromString(value string) Friend {
 }
 
 type Database interface {
-	AddFriend()
-	GetFriends()
-	SetFriendAsContacted()
+	AddFriend(friend Friend)
+	GetAllFriends() []Friend
+	SetFriendAsContacted(friend Friend)
 }
 
 type RedisDatabase struct {
 	client redis.Client
 }
 
+func GetDatabase() Database {
+	// This will return the correct database based on settings
+	// At the moment only Redis is allowed, so we'll just return a Redis database
+	return NewRedisDatabase()
+}
+
 func ExampleRedis() {
 	redis := NewRedisDatabase()
-	friend := NewFriendFromString(data)
-	redis.AddFriend(friend)
 	friends := redis.GetAllFriends()
 	fmt.Println(friends)
 }
@@ -80,9 +109,9 @@ func ExampleRedis() {
 func NewRedisDatabase() RedisDatabase {
 	url := os.Getenv("REDIS_URL")
 	client := redis.NewClient(&redis.Options{
-		Addr:			url,
-		Password:		"",
-		DB:				0,
+		Addr:     url,
+		Password: "",
+		DB:       0,
 	})
 	db := RedisDatabase{
 		client: *client,
@@ -101,10 +130,22 @@ func (r RedisDatabase) GetAllFriends() []Friend {
 	}
 
 	var friends []Friend
-	for _, value := range result {
-		friends = append(friends, NewFriendFromString(value))
+	var friend Friend
+	for i, value := range result {
+		friend = NewFriendFromString(value)
+		friend.id = int64(i)
+		friends = append(friends, friend)
 	}
 
 	return friends
-	
+}
+
+func (r RedisDatabase) SetFriendAsContacted(friend Friend) {
+	friend.DateContacted = MyTime(time.Now())
+	mess, err := json.Marshal(friend)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Putting", friend.Name, "as contacted:", string(mess))
+	r.client.LSet(redisFriendListName, friend.id, mess)
 }

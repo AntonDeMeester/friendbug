3868ee6797f477aa32f23748cb99cf9d9c95b75d
Anton <antondm@hotmail.com>
2019-07-04 21:12:00 +0200

    Add contact frequency to the date contacted check (duh)

--- cmd/friendbug/main.go
+++ cmd/friendbug/main.go
@@ -22,7 +22,7 @@ func ContactFriends() {
 
 	for _, friend := range friends {
 		// Take 23 hour to not interfere with exact time differences.
-		if friend.DateContacted.Before(time.Now().Add(time.Hour * -23)) {
+		if friend.DateContacted.Before(time.Now().Add(time.Hour * -23).Add(time.Day * -1 * friend.ContactFrequency)) {
 			toContactFriends = append(toContactFriends, friend)
 		} 
 	}
